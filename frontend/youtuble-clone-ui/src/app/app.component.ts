b2f53142d61977118d0725ab4b214f9376958c7e
geovanny.nieves <geovanny.nieves@globant.com>
2025-02-28 02:24:15 -0500

    frontend (navigation): implements navigation through the side bar

--- frontend/youtuble-clone-ui/src/app/app.component.ts
+++ frontend/youtuble-clone-ui/src/app/app.component.ts
@@ -2,21 +2,26 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolBarHeaderComponent } from "./header/header.component";
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolBarHeaderComponent],
+  imports: [
+    RouterOutlet,
+    ToolBarHeaderComponent,
+    SidebarComponent,
+    MatSidenavModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   constructor(private oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit() {
-    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated}) => {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated }) => {
       console.log('is auth?', isAuthenticated);
     });
   }
-
-
 }
