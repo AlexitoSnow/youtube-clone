38e639ee6a30f09b646b5595922ab9e0231a4f9f
geovanny.nieves <geovanny.nieves@globant.com>
2025-03-01 02:46:37 -0500

    backend: update security config

--- backend/src/main/java/com/globant/youtube_clone/config/SecurityConfig.java
+++ backend/src/main/java/com/globant/youtube_clone/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration @EnableWebSecurity
 public class SecurityConfig {
@@ -14,14 +15,16 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(requests -> {
                     requests
-                            .requestMatchers("/api/videos", "/api/videos/{videoId}")
+                            .requestMatchers(
+                                    "/api/videos", "/api/videos/{videoId}",
+                                    "/api/user/{userId}", "api/videos/{videoId}/comments")
                             .permitAll(); // Permitir acceso sin autenticación
                     requests.anyRequest().authenticated();
                 }).oauth2ResourceServer(
                         oauth2 -> {
                             oauth2.jwt(Customizer.withDefaults());
                         }
-                ).csrf(Customizer.withDefaults())
+                ).csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .cors(Customizer.withDefaults());
         return http.build();
     }
